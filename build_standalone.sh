02f5492c80b27a4c8f248bad947d3e0cdd634a12
JaidenAlemni <jaiden@studioalemni.com>
2022-11-22 10:18:46 -0500

    windows build shortcuts

---
+++ build_standalone.sh
@@ -0,0 +1,4 @@
+cd build
+meson configure -Dsteamworks_path="" -Dsteam_appid="" -Dhide_console=false
+ninja
+mv mkxp-z.exe Game.exe
---
+++ build_steam.sh
@@ -0,0 +1,3 @@
+cd build
+meson configure -Dsteamworks_path="C:/Ruby30-x64/msys64/home/Liz/mkxp-z-astravia/steam/sdk" -Dsteam_appid="1504930" -Dhide_console=true
+ninja
